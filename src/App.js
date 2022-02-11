8f6103ca9e92d01bf81cb514b679e0c86a2d2adc
Rbrown234 <66231958+Rbrown234@users.noreply.github.com>
2022-02-11 17:53:35 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import shoes1 from './pexels-photo.jpg';
+import shoes2 from './blackshoe.jpg';
+import shoes3 from './pjimage-1-9.webp';
+import shoes4 from './boots-3.png';
 import './App.css';
+import raw from "./ToS.txt"
+import { useState } from "react";
+import { Route, IndexRoute } from 'react-router';
+import background from './cloud-background.jpg'
+
+
+
+
+
+
+
+//var __html = require('./ToS.html');
+//var template = { __html: __html };
+
+
 
 function App() {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [checked, setChecked] = useState(false);
+
+  const canBeSubmitted = () => {
+    return checked ? setIsDisabled(true) : setIsDisabled(false);
+  };
+
+  const onCheckboxClick = () => {
+    setChecked(!checked);
+    return canBeSubmitted();
+  };
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      <div style={{backgroundImage:`url(${background})`}}>
+        <h1>
+          Welcome to Cob's! 
+        </h1>
+      </div>
+
+        
+        
       </header>
+      <div id="pagebody">
+        <div id="shoe_pictures">
+          
+          <img src={shoes1} alt="shoes" class="shoe-image"></img>
+          <img src={shoes2} alt="shoes" class="shoe-image"></img>
+          <img src={shoes3} alt="shoes" class="shoe-image"></img>
+          
+        </div>
+        
+        <div id="termsofservice">
+            <input type="checkbox" onClick={onCheckboxClick} />
+          I accept Cob's Terms of Service and Privacy Policy 
+        <object width="300" height="250" type="text/plain" data={raw} border="0"></object>
+            <button type="submit" disabled={isDisabled}>
+              Continue
+          </button>
+        </div>
+        
+        <div id="email_input">
+          <form>
+            <label>
+              Please input your email to receive newsletters:
+              <input type="text" name="email" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+
+          
+        
+
+        <div id="moreshoes">
+          <img src={shoes4} alt="shoes" class="shoe-image"></img>
+        </div>
+
+      </div>
+      
     </div>
   );
 }
